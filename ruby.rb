c3bf008d7d83f919b24ab0dd9168107b158e124c
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-07-28 14:37:33 +0000

    ruby復習

---
+++ ruby.rb
@@ -0,0 +1,13 @@
+# puts "■ ■ ■ ■ ■ ■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■ ■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■ ■"
+# puts "■ ■ ■ ■"
+# puts "■ ■ ■"
+# puts "■ ■"
+# puts "■"
+
+User = Struct.new(:name, :age)
+tom = User.new("Tom", 30)
